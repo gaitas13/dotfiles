f39ebbbf161f242133da207c01af3157722ffe8c
gaitas13 <91051922+gaitas13@users.noreply.github.com>
2023-12-23 18:12:04 +0100

    updated vanilla emacs config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,8 +1,8 @@
-(org-babel-load-file
- (expand-file-name
-  "config.org"
-  user-emacs-directory))
-  (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+;;(org-babel-load-file
+;; (expand-file-name
+;;  "config.org"
+;;  user-emacs-directory))
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 (load-theme 'catppuccin t)
 (setq catppuccin-flavor 'frappe) ;; or 'latte, 'macchiato, or 'mocha
 (catppuccin-reload)
