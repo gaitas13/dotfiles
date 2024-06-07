6256d43c8a5c7643ba1f5413bd84c244b64c3619
gaitas13 <91051922+gaitas13@users.noreply.github.com>
2024-06-07 12:38:24 +0200

    updated mpv config

--- .config/doom/custom.el
+++ .config/doom/custom.el
@@ -4,14 +4,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(+org-capture-todo-file " t)
  '(custom-safe-themes
    '("944d52450c57b7cbba08f9b3d08095eb7a5541b0ecfb3a0a9ecd4a18f3c28948" "1a1ac598737d0fcdc4dfab3af3d6f46ab2d5048b8e72bc22f50271fd6d393a00" default))
  ;;'(gdscript-godot-executable "godot")
  '(lsp-gdscript-port 6005)
  '(lsp-tcp-connection-timeout 10)
- '(org-agenda-files
-   '("" ""))
  '(org-capture-templates
    '(("t" "Personal todo" entry
       (file+headline +org-capture-todo-file "TODOS")
